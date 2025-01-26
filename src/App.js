d7366dfde6911d637e1128634886ff4ca38335b6
Aman RK <85789009+Amanrk7@users.noreply.github.com>
2025-01-26 23:58:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ const App = () => {
  
 
   return (
-    <>
-     
-
-        div className="footer">
-      <div className="footer-child1">
-        <div className="footer-child1-2">MAYA INCOPRATION</div>
-        <div className="footer-child1-1">
-          HNO. 303 GANESH GALAXY CITY BUILDISNG AYODHYA BYPASS ROAD BHOPAL
-          462041Dist.-BHOPAL
-        </div>
-      </div>
-
-      <div className="footer-child2">Artium Hive, all rights reserved</div>
-    </div>
+  <div style={{position:"fixed", bottom:"0", width:"100%", backgroundColor:"white"}>
+   Artium Hive, all rights reserved
+   </div>
 };
 
 export default App;
